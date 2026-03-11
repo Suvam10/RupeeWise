c3d0ff9076d701749cfc8d5cb3d4eecec07d17da
Suvam Debnath <146423407+Suvam10@users.noreply.github.com>
2026-03-11 14:18:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/RupeeWise/"
-})
+  base: "/RupeeWise/",
+  build: {
+    outDir: "dist",
+  },
+});
